2acd90252463077e0d07d81a8a9444606701e64d
RohanJoshuaAsveen <rohanjoshuaasveen@gmail.com>
2021-07-09 23:53:41 +0530

    Added Code Solution

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -1,8 +1,15 @@
 public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
-		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
+		if((square/8)%2==0) {
+			if(square%2==0)
+				return false;
+		}
+		else{
+			if(square%2==1)
+				return false;
+		}
+		
 		return true;
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -3,33 +3,56 @@ public class GameStrategy {
 	private int numQueens = 0;
 
 	private int getColumn(int cellId) {
-		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		return cellId%8;
 	}
 	
 	private int getRow(int cellId) {
-		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		return cellId/8;
 	}
 
 	public boolean isValidPosition(int cellId) {
 		boolean isValid = true;
 
-		if(numQueens == 8) throw new GameOverException();	// just return out of the method
-
-		// the game is still on, so let us continue...
+		if(numQueens == 8) throw new GameOverException();
 		
 		int col = getColumn(cellId);
 		
 		int row = getRow(cellId);
 		
-		/*
-			WRITE YOUR LOGIC HERE...............................
-
-		*/
-		return isValid;
+		//Check for queen in row and columns
+		for(int i=0; i<8; i++) {
+			if(placedQueens[i][col] || placedQueens[row][i])
+				return false;
+		}
+		
+		//Check for queen in upper left diagonal
+		for(int i=row,j=col; i>=0 && j>=0;i--, j--){
+			if(placedQueens[i][j])
+				return false;
+		}
+		
+		//Check for queen in upper right diagonal
+		for(int i=row,j=col; i>=0 && j<8;i--, j++){
+			if(placedQueens[i][j])
+				return false;
+		}
+		
+		//Check for queen in lower left diagonal
+		for(int i=row,j=col; i<8 && j>=0;i++, j--){
+			if(placedQueens[i][j])
+				return false;
+		}
+		
+		//Check for queen in lower right diagonal
+		for(int i=row,j=col; i<8 && j<8;i++, j++){
+			if(placedQueens[i][j])
+				return false;
+		}
+		
+		
+		placedQueens[row][col] = true;
+		numQueens++;
+		return true;
 	}
 }
 
